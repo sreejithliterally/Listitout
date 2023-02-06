0161cc492c07b4a6961fcd557a0de3044b7612a9
sreejith <imsreejithofficial@gmail.com>
2023-02-07 01:28:35 +0530

    add input

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import InputField from './components/InputField';
 import './App.css';
 
 
-const App=()=>{
+const App: React.FC=()=>{
+
+
+  const [todo, setTodo] = useState<string>("")
+  console.log(todo);
   return (
   <div className="App">
 
   <span className="heading"> Our Bucket List </span>
-  <InputField />
+  <InputField todo={todo} setTodo={setTodo}/>
   </div>
   );
 }
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import "./styles.css"
 
-const InputField = () => {
+interface Props{
+  todo:string;
+  setTodo:React.Dispatch<React.SetStateAction<string>>;
+}
+const InputField = ({todo,setTodo}:Props) => {
   return (
-    <div>
-      
-    </div>
+    <form className='input'>
+    <input type='input' 
+    value={todo}
+    onChange={
+      (e)=>setTodo(e.target.value)
+    }
+    placeholder='Enter a wish' className='input_box'></input>
+    <button className='input_submit'type='submit'> GO</button>
+    
+    </form>
   )
 }
 
